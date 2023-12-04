5a2982d9979a3d7f54f4a4256050c5243e50254b
MiguelSan7 <134112758+MiguelSan7@users.noreply.github.com>
2023-12-04 10:57:14 -0600

    primero

---
+++ app/Http/Controllers/SensorsController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SensorsController extends Controller
+{
+    //
+}
---
+++ app/Models/Collar.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Collar extends Model
+{
+    use HasFactory;
+}
