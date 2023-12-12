6cf2b5e9ea7597be6950190e081c18c69b1ee8a6
MiguelSan7 <134112758+MiguelSan7@users.noreply.github.com>
2023-12-12 12:50:48 -0600

    guzzle

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\PetsController;
+use App\http\Controllers\GuzzleController;
 
 /*
 |--------------------------------------------------------------------------
